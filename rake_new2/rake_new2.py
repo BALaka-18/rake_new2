3c880dd2981a09be73162fe594cb10dc82be3791
Balaka Biswas <49288068+BALaka-18@users.noreply.github.com>
2020-07-31 18:55:26 +0530

    Commit main

--- rake_new2/rake_new2.py
+++ rake_new2/rake_new2.py
@@ -1,10 +1,11 @@
 # Import libraries
 import string
+import re
 from collections import Counter,defaultdict
 from itertools import groupby,product,chain
 import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
+'''nltk.download('stopwords')
+nltk.download('punkt')'''
 from nltk.tokenize import wordpunct_tokenize
  
 '''WHY USE wordpunct_tokenize INSTEAD OF word_tokenize ?
@@ -17,7 +18,7 @@ Hence, wordpunct_tokenize > word_tokenize'''
 
 # the Rake() class
 class Rake():
-  def __init__(self,stopwords=None,punctuations=None,language='english'):
+  def __init__(self,stopwords=None,punctuations=None,language='english',keep_html_tags = False):
     # Stopwords
     self.stopwords = stopwords
     if self.stopwords is None:
@@ -26,9 +27,12 @@ class Rake():
     self.punctuations = punctuations
     if self.punctuations is None:
       self.punctuations = string.punctuation
+    # HTML Tags : If a sentence contains HTML tags, you may choose whether to keep the tag names as keywords.
+    # Default : False
+    self.keep_html_tags = keep_html_tags
 
     # Clubbing all together into one list of indicators where text will be split
-    self.ignore_list = set(chain(self.stopwords,self.punctuations))
+    self.ignore_list = list(set(chain(self.stopwords,self.punctuations)))
     # Initializing the calculation metrics
     self.freq_dist = None       # Frequency distribution of each word in the text
     self.word_degree = None     # Degrees of keywords
@@ -37,8 +41,14 @@ class Rake():
   
   # Extract keywords
   def get_keywords_from_raw_text(self,text):
-    sentences = nltk.tokenize.sent_tokenize(text)
-    self.get_keywords_from_sent(sentences)   # _______(1)
+    if self.keep_html_tags:
+      sentences = nltk.tokenize.sent_tokenize(text)
+      self.get_keywords_from_sent(sentences)   # _______(1)
+    else:
+      cleanr = re.compile('<.*?>')
+      cleantext = re.sub(cleanr, '', text)
+      sentences = nltk.tokenize.sent_tokenize(cleantext)
+      self.get_keywords_from_sent(sentences)   # _______(1)
   # (1)
   def get_keywords_from_sent(self,sentences):
     phrase_list = self.get_keywords(sentences)    # ________(2)
@@ -55,7 +65,18 @@ class Rake():
     return phrases
   # (6) --> USER ACCESSIBLE FUNCTION
   def get_keywords_from_words(self,words):
-    filtered_groups = groupby(chain.from_iterable(words),lambda x : x not in self.ignore_list)
+    word_list = list(chain.from_iterable(words))
+    ignore_final = []
+    for elem in word_list:
+      temp = list(elem)
+      for val in temp:
+        if val in string.punctuation:
+          ignore_final.append(elem)
+          break
+        else:
+          continue
+    ignore_final = ignore_final + self.ignore_list
+    filtered_groups = groupby(word_list,lambda x : x not in ignore_final)
     keywords_list = [list(grp[1]) for grp in filtered_groups if grp[0]]
     return keywords_list
   # (3)
@@ -82,13 +103,13 @@ class Rake():
         rank += self.degree[word]/self.freq_dist[word]
       self.ranklist.append((rank, " ".join(kw)))
     # Sorting in descending order of ranks and preparing keywords + scores
-    self.ranklist.sort(reverse=True)
     self.ranked_kw = [kwd[1] for kwd in self.ranklist]
+
 
   # USER ACCESSIBLE FUNCTIONS
   # (A)
   def get_ranked_keywords(self):
-    return self.ranked_kw
+    return set(self.ranked_kw)
   # (B)
   def get_keywords_with_scores(self):
     return set(self.ranklist)
@@ -98,9 +119,11 @@ class Rake():
   # (D)
   def get_kw_degree(self):
     return self.degree
+  def p(self):
+    print()
 
 
-'''# Test run
+'''# Test run cases
 text = "Red apples are good in taste."
 rake = Rake()
 rake.get_keywords_from_raw_text(text)
@@ -108,4 +131,31 @@ kw_s = rake.get_keywords_with_scores()
 kw = rake.get_ranked_keywords()
 f = rake.get_word_freq()
 deg = rake.get_kw_degree()
-print(deg)'''
+print(deg)
+text = "<h1> Hello world !</h1>"
+text2 = "<span> Hi Rahul. Hello rahul, say hi to rahul.</span><p> See, Rahul says hello, you should say hello to rahul !</p>"
+text3 = "Keyword extraction is not that difficult after all. There are many libraries that can help you with keyword extraction. Rapid automatic keyword extraction is one of those."
+
+rake1,rake2,rake3,rake4,rake5 = Rake(keep_html_tags=True),Rake(keep_html_tags=False),Rake(keep_html_tags=True),Rake(keep_html_tags=False),Rake(keep_html_tags=False)
+print("\nTYPE I : ORIGINAL TEXT : {}".format(text))
+# Case 1
+rake1.get_keywords_from_raw_text(text)
+kw_s1 = rake1.get_keywords_with_scores()
+kw1 = rake1.get_ranked_keywords()
+print("Keeping the tags : ",kw1)
+# Case 2
+rake2.get_keywords_from_raw_text(text)
+kw_s2 = rake2.get_keywords_with_scores()
+kw2 = rake2.get_ranked_keywords()
+print("Eliminating the tags : ",kw2)
+print("\nTYPE II : ORIGINAL TEXT : {}".format(text2))
+# Case 3
+rake3.get_keywords_from_raw_text(text2)
+kw_s3 = rake3.get_keywords_with_scores()
+kw3 = rake3.get_ranked_keywords()
+print("Keeping the tags : ",kw3)
+# Case 4
+rake4.get_keywords_from_raw_text(text2)
+kw_s4 = rake4.get_keywords_with_scores()
+kw4 = rake4.get_ranked_keywords()
+print("Eliminating the tags : ",kw4)'''
