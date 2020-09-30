8457a8006738ef4f21c5dc777e8815d9b0493a46
Restyled.io <commits@restyled.io>
2020-09-30 19:35:49 +0000

    Restyled by black

--- tests/tfidf_scratch_vs_rake.py
+++ tests/tfidf_scratch_vs_rake.py
@@ -3,6 +3,7 @@ from timeit import default_timer as timer
 from rake_new2 import Rake
 from tfidf_vectorizer.extract_keywords_tfidf_scratch import TF_IDF_Scratch
 import sys
+
 sys.path.append("..")
 
 
@@ -11,11 +12,28 @@ docs = [
     "C++ is a general purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or 'C with Classes'. The language has expanded significantly over time, and modern C++ now has object oriented, generic, and functional features in addition to facilities for low-level memory manipulation.",
     "Python is an interpreted, high level and general purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object oriented approach aim to help programmers write clear, logical code for small and large scale projects.",
     "Haskell is a general purpose, statically typed, purely functional programming language with type inference and lazy evaluation. Developed to be suitable for teaching, research and industrial application, Haskell has pioneered a number of advanced programming language features such as type classes, which enable type safe operator overloading.",
-    "Kotlin is a cross platform, statically typed, general purpose programming language with type inference. Kotlin is designed to interoperate fully with Java, and the JVM version of Kotlin's standard library depends on the Java Class Library, but type inference allows its syntax to be more concise. Kotlin mainly targets the JVM, but also compiles to JavaScript or native code (via LLVM), e.g. for native iOS apps sharing business logic with Android apps. Language development costs are borne by JetBrains, while the Kotlin Foundation protects the Kotlin trademark."
+    "Kotlin is a cross platform, statically typed, general purpose programming language with type inference. Kotlin is designed to interoperate fully with Java, and the JVM version of Kotlin's standard library depends on the Java Class Library, but type inference allows its syntax to be more concise. Kotlin mainly targets the JVM, but also compiles to JavaScript or native code (via LLVM), e.g. for native iOS apps sharing business logic with Android apps. Language development costs are borne by JetBrains, while the Kotlin Foundation protects the Kotlin trademark.",
 ]
 
-manual_keyphrases = ['python', 'guido van rossum', 'first released', '1991', 'notable', 'interpreted', 'high level', 'general purpose',
-                     'design philosophy', 'code readability', 'whitespace', 'language constructs', 'object oriented', 'clear', 'logical', 'small', 'large scale projects']
+manual_keyphrases = [
+    "python",
+    "guido van rossum",
+    "first released",
+    "1991",
+    "notable",
+    "interpreted",
+    "high level",
+    "general purpose",
+    "design philosophy",
+    "code readability",
+    "whitespace",
+    "language constructs",
+    "object oriented",
+    "clear",
+    "logical",
+    "small",
+    "large scale projects",
+]
 
 start_tfidf = timer()
 tfidf = TF_IDF_Scratch()
@@ -28,10 +46,10 @@ rake.get_keywords_from_raw_text(docs[2])
 rake_res = rake.get_ranked_keywords()
 end_rake = timer()
 
-print('tf-idf (scratch) results: ')
+print("tf-idf (scratch) results: ")
 print(tfidf_res)
 print()
-print('rake results: ')
+print("rake results: ")
 print(rake_res)
 print()
 
@@ -48,10 +66,9 @@ for keyphrase in manual_keyphrases:
 tfidf_precision = tfidf_tp / tfidf_total * 100
 rake_precision = rake_tp / rake_total * 100
 
-print('tf-idf (scratch) precision score: ' +
-      str(round(tfidf_precision, 2)) + '%')
-print('rake precision score: ' + str(round(rake_precision, 2)) + '%')
+print("tf-idf (scratch) precision score: " + str(round(tfidf_precision, 2)) + "%")
+print("rake precision score: " + str(round(rake_precision, 2)) + "%")
 print()
 
-print('tf-idf (scratch) time: ' + str(round(end_tfidf - start_tfidf, 3)) + 's')
-print('rake time: ' + str(round(end_rake - start_rake, 3)) + 's')
+print("tf-idf (scratch) time: " + str(round(end_tfidf - start_tfidf, 3)) + "s")
+print("rake time: " + str(round(end_rake - start_rake, 3)) + "s")
