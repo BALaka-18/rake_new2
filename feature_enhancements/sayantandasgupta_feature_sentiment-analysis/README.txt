78af178c7c356ce8e537ad900358998414ecd510
Sayantan Dasgupta <61374798+Arka2001@users.noreply.github.com>
2021-01-06 10:14:19 +0530

    Sentiment Analysis Initial Code (#36)
    * Sentiment Analysis Initial Code
    
    * New Features added for Sentiment Analysis

---
+++ feature_enhancements/sayantandasgupta_feature_sentiment-analysis/README.txt
@@ -0,0 +1,30 @@
+14/12/2020 
+
+Setup the local environment for development. 
+Started to work on the preprocessing part of the dataset. The dataset is being used for the Word2Vec model.
+
+16/12/2020
+
+Completed the preprocessing part for the dataset. The entire corpus is ready to train and create the Word2Vec model for sentiment analysis. 
+
+18/12/2020
+
+Created an initial sentiment analysis feature for the package, by using the VADER sentiment analysis tool. This tool had been used for analysing the sentiments of short sentences and sentences containing emojis and short forms
+
+19/12/2020
+
+Trained and created the Word2Vec model for further use to cluster and predict the sentiments of user-provided texts.
+
+23/12/2020
+
+Created the Word2Vec model on the corpus that had been created previously on the text corpus.
+
+24/12/2020
+
+Trained the Word2Vec model on the corpus. Started work on the clustering on the model.
+
+02/01/2021
+Created the clusters using sklearn's KMeans clustering and calculated the approximate sentiment coefficients for different keywords in the model.
+
+03/01/2021
+Completed the prediction on the entire corpus of the input text
---
+++ feature_enhancements/sayantandasgupta_feature_sentiment-analysis/model.py
@@ -0,0 +1,59 @@
+import re
+from re import sub
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize,sent_tokenize
+from gensim.models import Word2Vec
+import multiprocessing
+
+def text_preprocess(text):
+    text = str(text)
+    text = text.lower()
+
+    stop_words = stopwords.words('english')
+
+    #clean the text
+
+    text = sub(r"[^A-Za-z0-9^,!?.\/'+]", " ",text)
+    text = sub(r"\+","plus",text)
+    text = sub(r","," ",text)
+    text = sub(r"\.", " ", text)
+    text = sub(r"!", " ! ", text)
+    text = sub(r"\?", " ? ", text)
+    text = sub(r"'", " ", text)
+    text = sub(r":", " : ", text)
+    text = sub(r"\s{2,}", " ", text)
+    text = sub(r"\n","",text)
+    text = word_tokenize(text)
+    text = [word for word in text if word not in stop_words]
+
+    return text
+
+def file_preprocess():
+    file1 = open(r"E:\big.txt","r")
+    sent_list = file1.readlines()
+    sent_list = [re.sub(r"\n"," ",x) for x in sent_list]
+
+    word_vec = [text_preprocess(sent) for sent in sent_list]
+    word_vec = [sent for sent in word_vec if len(sent)>0]
+    return word_vec
+
+
+def build_model():
+
+    word_vector = file_preprocess()
+
+    cores = multiprocessing.cpu_count()
+
+    model = Word2Vec(min_count=20,window=2,size=300,sample=6e-5,alpha=0.03,min_alpha=0.0007,negative=20,workers=cores-1)
+
+    model.build_vocab(word_vector,progress_per=10000)
+
+    model.train(word_vector,total_examples=model.corpus_count,epochs=30,report_delay=1)
+
+    model.init_sims(replace=True)
+
+    model.save("word2vec.model")
+
+build_model()
